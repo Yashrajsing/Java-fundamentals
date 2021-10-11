0db1165efc9ef09292117181a9e7e6fcbfbc68a9
Yashrajsing <92075820+Yashrajsing@users.noreply.github.com>
2021-10-11 12:19:22 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,18 @@
+package precourse;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		for(int num = 1 ; num<=100 ; num = num + 1 ){
+	        System.out.println(num);
+	       }
+	        int num = 100;
+	        while (num <= 100){
+	            System.out.println(num);
+	            num = num + 1;
+
+	        }
+
+	}
+
+}
---
+++ Oddeven.java
@@ -0,0 +1,16 @@
+package precourse;
+
+public class Oddeven {
+
+	public static void main(String[] args) {
+int num = 13;
+		
+		if (num % 2 == 0) {
+			System.out.print("Even number");
+		}else {
+			System.out.print("Odd number");
+		}
+
+	}
+
+}
